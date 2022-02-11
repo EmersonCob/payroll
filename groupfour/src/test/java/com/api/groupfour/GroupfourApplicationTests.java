66a235631822b664604eb676de6632b89912af28
Jose Emerson <jose.batistacosta@everis.nttdata.com>
2022-02-11 09:17:54 -0300

    ms-payroll

---
+++ groupfour/src/test/java/com/api/groupfour/GroupfourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.groupfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupfourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
